a21464cd07f6cd38ae49ca00839f130d12d4f861
jaanus <jaanus@gmail.com>
2013-09-20 23:06:56 +0200

    API for specifying image download policy. Posts path is configurable.

--- tumblr2markdown.py
+++ tumblr2markdown.py
@@ -4,10 +4,23 @@
 import pytumblr
 from datetime import datetime # for strptime
 import re
+import os
 import codecs
 import argparse
 
-def downloader(apiKey, host):
+
+
+def processPostBodyForImages(postBody, downloadImages, imagesPath):
+
+	if not downloadImages:
+		return postBody
+
+	print "should process body here"
+	return postBody
+
+
+
+def downloader(apiKey, host, postsPath, downloadImages, imagesPath):
 
 	# Authenticate via API Key
 	client = pytumblr.TumblrRestClient(apiKey)
@@ -72,6 +85,9 @@ def downloader(apiKey, host):
 
 				print post
 
+			# download images if required
+			body = processPostBodyForImages(body, downloadImages, imagesPath)
+
 			# we have processed the post, have "title" and "body" by now, let’s dump it on disk
 
 			# generate a slug out of the title: replace weird characters …
@@ -86,7 +102,11 @@ def downloader(apiKey, host):
 			# … and prepend date
 			slug = postDate.strftime("%Y-%m-%d-") + slug + ".markdown"
 
-			f = codecs.open("_posts/" + slug, encoding='utf-8', mode="w")
+			# if path does not exist, make it
+			if not os.path.exists(postsPath):
+				os.makedirs(postsPath)
+
+			f = codecs.open(os.path.join(postsPath, slug), encoding='utf-8', mode="w")
 
 			tags = ""
 			if len(post["tags"]):
@@ -101,26 +121,31 @@ def downloader(apiKey, host):
 	print "Posts per type:", posts_per_type
 
 
+
 def main():
 	
 	parser = argparse.ArgumentParser(description="Tumblr to Markdown downloader",
 		epilog = """
-		This app downloads all your Tumblr content into Markdown files that are suitable for processing with Octopress.
+		This app downloads all your Tumblr content into Markdown files that are suitable for processing with Octopress. It optionally also downloads the images hosted on Tumblr and replaces their URLs with locally hosted versions.
 		""")
 	parser.add_argument('--apikey', dest="apiKey", required=True, help="Tumblr API key")
 	parser.add_argument('--host', dest="host", required=True, help="Tumblr site host, e.g example.tumblr.com")
+	parser.add_argument('--posts-path', dest="postsPath", default="_posts", help="Output path for posts, by default “_posts”")
+	parser.add_argument('--download-images', dest="downloadImages", action="store_true", help="Whether to download images hosted on Tumblr into a local folder, and replace their URLs in posts")
+	parser.add_argument('--images-path', dest="imagesPath", default="images", help="If downloading images, store them to this local path, by default “images”")
 
 	args = parser.parse_args()
 
 	if not args.apiKey:
-		print "API key is required."
+		print "Tumblr API key is required."
 		exit(0)
 
 	if not args.host:
 		print "Tumblr host name is required."
 		exit(0)
 
-		downloader(args.apiKey, args.host)
+	downloader(args.apiKey, args.host, args.postsPath, args.downloadImages, args.imagesPath)
+
 
 
 if __name__ == "__main__":
